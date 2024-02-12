5e56373097ecb5e364678e7e085778197a13411e
DPBM19 <159782206+DPBM19@users.noreply.github.com>
2024-02-12 13:42:44 +0100

    Update create_streamlift_app.py

--- create_streamlift_app.py
+++ create_streamlift_app.py
@@ -2,5 +2,5 @@ import streamlit
 streamlit.title('My parents new healthy diner')
 streamlit.header('breakfast menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
-               streamlit.text('Kale, spinach, rocket smoothie')
-  streamlit.text('hard boiled free range egg')
+streamlit.text('Kale, spinach, rocket smoothie')
+streamlit.text('hard boiled free range egg')
