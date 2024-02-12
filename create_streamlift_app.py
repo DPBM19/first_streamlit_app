a82f4b0d2d108709903cc66a78eba06dc43024b8
DPBM19 <159782206+DPBM19@users.noreply.github.com>
2024-02-12 13:44:14 +0100

    Update create_streamlift_app.py
    added emojist and texts

--- create_streamlift_app.py
+++ create_streamlift_app.py
@@ -1,6 +1,8 @@
 import streamlit
 streamlit.title('My parents new healthy diner')
 streamlit.header('breakfast menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, spinach, rocket smoothie')
-streamlit.text('hard boiled free range egg')
+streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗Kale, spinach, rocket smoothie')
+streamlit.text('🐔hard boiled free range egg')
+streamlit.text(' 🥑🍞avocado toast')
+  
