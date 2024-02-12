f139280a67b66ce94753235a03c86da8532bb229
DPBM19 <159782206+DPBM19@users.noreply.github.com>
2024-02-12 13:48:54 +0100

    Update create_streamlift_app.py
    added import pandas
    added import of .csv, fruit list.

--- create_streamlift_app.py
+++ create_streamlift_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas
 streamlit.title('My parents new healthy diner')
 streamlit.header('breakfast menu')
 streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
@@ -6,4 +7,5 @@ streamlit.text('🥗Kale, spinach, rocket smoothie')
 streamlit.text('🐔hard boiled free range egg')
 streamlit.text(' 🥑🍞avocado toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-  
+my_fruits_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
