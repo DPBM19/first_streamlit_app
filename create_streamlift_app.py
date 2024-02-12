6907a529c1f5732ac8580720e3fc3b03ef12d2af
DPBM19 <159782206+DPBM19@users.noreply.github.com>
2024-02-12 14:04:54 +0100

    Update create_streamlift_app.py
    added fruits to show
    added fruit list loc

--- create_streamlift_app.py
+++ create_streamlift_app.py
@@ -11,6 +11,8 @@ import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 #Lets use a pick list so they can pick the fruit they want to include 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberry'])
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberry'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
+streamlit.dataframe(fruits_to_show)
 #display on the page
 #streamlit.dataframe(my_fruit_list)
